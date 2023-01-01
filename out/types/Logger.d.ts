b0624b2b53e5ae91fd8e1ca9e49fd86cd4462327
therealemissions <me@liammartin.site>
2023-01-01 02:32:33 +0000

    updated

---
+++ out/types/Logger.d.ts
@@ -0,0 +1,8 @@
+declare class Logger {
+    private static getTimestamp;
+    private static getDateString;
+    static internalError(message: string, type?: string, ...optionalParams: any[]): void;
+    static userError(message: string, type?: string, ...optionalParams: any[]): void;
+    static log(message: string, type?: string, ...optionalParams: any[]): void;
+}
+export default Logger;
